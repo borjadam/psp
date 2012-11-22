c0465babb491dd217bb49691b8738958193a8be5
borja <bma.francisco@gmail.com>
2012-11-22 08:46:01 +0100

    Cambiando PGit

---
+++ src/PGit.java
@@ -0,0 +1,14 @@
+
+public class PGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola Mundo");
+		System.out.println("2");
+
+	}
+
+}
