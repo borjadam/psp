e5444f4f2ab0953a4a9320cb182b73d7d7c3e663
borja <bma.francisco@gmail.com>
2013-01-31 08:28:34 +0100

    añadiendo PRegex

--- HttpServer/src/serpis/psp/HttpServer.java
+++ HttpServer/src/serpis/psp/HttpServer.java
@@ -1,7 +1,11 @@
 package serpis.psp;
 
+import java.awt.HeadlessException;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -12,36 +16,85 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class HttpServer {
+	
+	private static String newLine= "\r\n";
 
 	/**
 	 * @param args
+	 * @throws InterruptedException 
 	 */
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, InterruptedException {
+		
 		int port = 8080;
-		String newLine= "\r\n";
-		
 		ServerSocket serverSocket = new ServerSocket(port);
-		Socket socket = serverSocket.accept();
 		
-		Scanner scanner = new Scanner(socket.getInputStream());
+		String threadName = Thread.currentThread().getName();
+		System.out.println(threadName);
+		
+		while (true){
+			Socket socket = serverSocket.accept();
+			String filename = getFileName(socket.getInputStream());
+			writeHeader(socket.getOutputStream(), filename);
+			writeFile(socket.getOutputStream(), filename);
+			socket.close();			
+		}
+		
+	}
+	
+	private static String getFileName(InputStream inputStream){
+		
+		Scanner scanner = new Scanner(inputStream);
+		String fileName = "";
 		
 		while(true) {
 			String line = scanner.nextLine();
-			if (line.equals("")){
+			System.out.println(line);
+			if (line.contains("GET ")){
+				String aux = line.substring(5);	
+				String[] aux2 = aux.split(" ");
+				fileName = aux2[0];
+				System.out.println("filename = " + fileName);
+			}
+			else if (line.equals("")){
 				break;
 			}
-			
-			System.out.println(line);
 		}
+		return fileName;		
+	}
+	
+	private static void writeHeader(OutputStream outputStream, String filename) throws IOException{
+		final String response200 = "HTTP/1.0 200 OK";
+		final String response404 = "HTTP/1.0 404 Not Found";
+		File file = new File(filename);
 		
-		PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
-		printWriter.println("HTTP/1.0 404 Not found" + newLine);
-		printWriter.println(newLine);
+		String response = file.exists() ? response200 : response404;		
 		
-		printWriter.flush();
+		String header = response + newLine + newLine;	
+		byte[] headerBuffer = header.getBytes();
 		
-		socket.close();
-		serverSocket.close();
+		outputStream.write(headerBuffer);
+		
+	}
+	
+	private static void writeFile(OutputStream outputStream, String filename) throws IOException, InterruptedException{
+		final String fileNmeError404 = "fileError404.html";
+		File file = new File(filename);
+		String responseFileName = file.exists() ? filename : fileNmeError404;
+		
+		final int bufferSize = 2048;
+		byte[] buffer = new byte[bufferSize];
+		
+		
+		FileInputStream fileInputStream = new FileInputStream(responseFileName);
+		
+		int count;
+		
+		while((count = fileInputStream.read(buffer)) != -1) {
+			outputStream.write(buffer, 0, count);
+			Thread.sleep(10000);
+		}		
+		
+		fileInputStream.close();		
 	}
 
 }
---
+++ HttpServer/src/serpis/psp/ThreadServer.java
@@ -0,0 +1,11 @@
+package serpis.psp;
+
+public class ThreadServer implements Runnable {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
